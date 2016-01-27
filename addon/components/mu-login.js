10c4e8c88f7f7f7ad38323148e7b5fc7ce0f5629
Erika Pauwels <erika.pauwels@tenforce.com>
2016-01-27 22:29:43 +0100

    Remove application specific logic

--- addon/components/mu-login.js
+++ addon/components/mu-login.js
@@ -4,11 +4,5 @@ import MuLoginMixin from 'ember-mu-login/mixins/mu-login';
 
 export default Ember.Component.extend(MuLoginMixin, {
   layout: layout,
-  classNames: ['mu-login'],
-  didInsertElement() {
-    Ember.$('body').addClass('login-body');
-  },
-  willDestroyElement() {
-    Ember.$('body').removeClass('login-body');
-  }
+  classNames: ['mu-login']
 });
