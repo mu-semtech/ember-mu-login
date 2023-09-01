f17e4fc0beace686d6f62bbe390a711df975bbb4
Rich <piemonkey@protonmail.ch>
2023-09-01 17:56:35 +0200

    Fix weird issue with import from simple-auth not working

--- app/authenticators/mu-semtech.js
+++ app/authenticators/mu-semtech.js
@@ -1,1 +1,4 @@
+// For some reason this import fails from inside the `addon` code, unless it is included somewhere
+// in the `app` directory, so why not here?
+import 'ember-simple-auth/authenticators/base';
 export { default } from 'ember-mu-login/authenticators/mu-semtech';
