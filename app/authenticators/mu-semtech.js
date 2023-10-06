354f782f11226ca2b8d092115c0bb0cc4e9f3fdc
Erika Pauwels <erika.pauwels@gmail.com>
2023-10-06 11:39:26 +0200

    Move ember-simple-auth from devDependencies to dependencies

--- app/authenticators/mu-semtech.js
+++ app/authenticators/mu-semtech.js
@@ -1,4 +1,1 @@
-// For some reason this import fails from inside the `addon` code, unless it is included somewhere
-// in the `app` directory, so why not here?
-import 'ember-simple-auth/authenticators/base';
 export { default } from 'ember-mu-login/authenticators/mu-semtech';
